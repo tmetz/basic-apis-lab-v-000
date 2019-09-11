625810991c2f61e449cf918f5dd56fb5ba5c74ae
Tammy Metz <tammy@tammymetz.com>
2019-09-11 17:59:51 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,8 @@ class RepositoriesController < ApplicationController
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['q'] = params[:query]
-      req.params['client_id'] = ENV["GITHUB_CLIENT"]
-      req.params['client_secret'] = ENV["GITHUB_SECRET"]
+      req.params['client_id'] = ENV['GITHUB_CLIENT']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
     end
     body = JSON.parse(@resp.body)
     if @resp.success?
