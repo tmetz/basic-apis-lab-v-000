3759963a36ab6c734d331651fdf61b93cf8775c4
Tammy Metz <tammy@tammymetz.com>
2019-09-11 17:49:44 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get '' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['q'] = params[:query]
       req.params['client_id'] = ENV[GITHUB_CLIENT]
       req.params['client_secret'] = ENV[GITHUB_SECRET]
