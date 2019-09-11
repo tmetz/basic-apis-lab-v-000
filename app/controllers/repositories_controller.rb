0637eba3ac4db17dabf754c673d88e09f5f737dd
Tammy Metz <tammy@tammymetz.com>
2019-09-11 17:42:50 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get '' do |req|
+      req.params['client_id'] = ENV[GITHUB_CLIENT]
+      req.params['client_secret'] = ENV[GITHUB_SECRET]
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @venues = body["info"]["name"]
+      name, html_url, description
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search'
   end
 end
